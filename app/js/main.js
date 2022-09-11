f50b00d5d9f49063134e4196674ace371f7988cf
AlexanderHiliou <gilevsasha17@gmail.com>
2022-09-11 15:48:07 +0300

    add blog

--- app/js/main.js
+++ app/js/main.js
@@ -21,6 +21,11 @@ $(function () {
     slidesToShow: 10,
     slidesToScroll: 10,
   });
+  $('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="./images/back-arrow.svg" alt="arrow left"</button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="./images/next-arrow.svg" alt="arrow right"</button>'
+    
+  });
 
   var mixer = mixitup('.gallery__inner', {
     load: {
